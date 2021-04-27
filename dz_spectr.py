00912764e3425d13065ffdc4735ce2b5c9143d39
imishin1 <gazolin2000@gmail.com>
2021-04-27 03:42:33 +0300

    Spectral characteristic of photosensitivity of the photodiode

--- dz_spectr.py
+++ dz_spectr.py
@@ -27,19 +27,11 @@ class Initial_param:
         self.x = 0.75 # mkm
         self.N = 7E15 # sm^-3
         self.talu = 1E-4 # s
-        #self.lambd = 0.6 # mkm
         self.N_plus = self.N * 100 # sm^-3
         self.S = 5E4 # sm/s
-        self.mu_n = 3900 # sm^2/(V*s)
-        self.mu_p = 1900 # sm^2/(V*s)
         self.epsilond = 16
         self.A = 1 # sm^2
         self.Eg = 0.67 # eV
-        self.lam = 1.24 / self.Eg
-        #self.n = 4 + 0.001106337 - 0.00314503 / self.lambd + 0.492812 / (self.lambd ** 2) - 0.601906 / (self.lambd ** 3) + 0.982897 / (self.lambd ** 4)
-        #self.Re = (self.n - 1) ** 2 / (self.n + 1) ** 2
-        #self.k = 1.4
-        #self.alfa = 4 * np.pi * self.k / (self.lambd * 1E-4)
         self.d = 2 # mkm
         self.I0 = 1E15
         self.U = 0
@@ -51,7 +43,7 @@ class Culculation_param:
     def __init__(self):
         Physical_quantities.__init__(self)
         Initial_param.__init__(self)
-    
+
     def find_n(self, lambd):
         return 4 + 0.001106337 - 0.00314503 / lambd + 0.492812 / (lambd ** 2) - 0.601906 / (lambd ** 3) + 0.982897 / (lambd ** 4)
 
@@ -70,20 +62,30 @@ class Culculation_param:
 class Draw_graph(Culculation_param):
     def draw(self):
         m = 1000
+
+        # Вводим область от 0.2 до 0.76 ввиду того, что апроксимация для функции параметра k была выведена для этих границ (сайт дает только такие)
+        lambd_min = 0.2
+        lambda_max = 0.76
+        lambda_step = 0.001
+        
+        # Задаем нулевые массивы для глубины и генерации, а также рассчитываем величину приращения dx
         x_list = List_of_param(m).new_list
         G_list = List_of_param(m).new_list
         dx = self.d * 1E-4 / (m - 1)
 
+        # Рассчитываем контактную разность потенциалов и ширину ОПЗ (в том числе с каждой стороны от x)
         fk = 0.026 * np.log(self.N * self.N_plus / (self.ni ** 2))
         Nb = self.N * self.N_plus/ (self.N + self.N_plus)
         Wp = ((2 * self.epsilond * self.epsilond_0 * Nb * (fk - self.U) / self.q) ** 0.5) / self.N_plus
         Wn = ((2 * self.epsilond * self.epsilond_0 * Nb * (fk - self.U) / self.q) ** 0.5) / self.N
         W = Wn + Wp
 
+        # Рассчитываем число индексов для каждой области (нужно будет, чтобы разбить на разные массивы каждой области)
         sp = int((self.x * 1E-4 - Wp) * m / (self.d * 1E-4))
         sn = int((self.d * 1E-4 - self.x * 1E-4 - Wn) * m / (self.d * 1E-4))
         sw = m - sn - sp
 
+        # Задаем необходимые нулевые массивы
         Gp_list = List_of_param(sp).new_list
         Gn_list = List_of_param(sn).new_list
         xn_list = List_of_param(sn).new_list
@@ -92,10 +94,8 @@ class Draw_graph(Culculation_param):
         dnp_list = List_of_param(sp).new_list
         dpn1_list = List_of_param(sn).new_list
         dnp1_list = List_of_param(sp).new_list
-        
-        lambd_list = list()
-        S_list = list()
 
+        # Разбиваем массив с общей генерацией и глубиной на частные массивы для p и n области
         Gp_list = G_list[0 : sp]
         Gn_list = G_list[m - sn : m]
         xp_list = x_list[0 : sp]
@@ -103,6 +103,10 @@ class Draw_graph(Culculation_param):
 
         dpn_list = Gn_list * self.talu
         dnp_list = Gp_list * self.talu
+
+
+        # --------------------------------------------------------------------------------------------------
+
 
         # Прогоночка p+
         a_list = List_of_param(sp).new_list
@@ -127,12 +131,15 @@ class Draw_graph(Culculation_param):
         d1_list[sp - 1] = 0
         r_list[sp - 1] = 0
 
-        for lambd in np.arange(0.2, 0.8, 0.1):
+        lambd_list = list()
+        Sn_list = list()
+
+        for lambd in np.arange(lambd_min, lambda_max, lambda_step):
             for i in range (0, m):
                 x_list[i] = i * dx
                 G_list[i] = self.find_G_x(x_list[i], self.find_k(lambd), lambd)
 
-            for j in range (0, 200):
+            for j in range (0, 1000):
                 for i in range (1, sp - 1):
                     d1_list[i] = 1
                     a_list[i] = -2 - (dx * dx / (self.dn * self.talu))
@@ -149,22 +156,21 @@ class Draw_graph(Culculation_param):
                     dnp_list[i] = de_list[i] * dnp_list[i + 1] + la_list[i]
 
                 dnp1_list = dnp_list
-                
+            
+            # Рассчитываем ток и относительную фоточувствительность. Формируем массивы спектра и фоточувствительности каждоый области
             k = self.find_k(lambd)
             alfa = self.find_alfa(k, lambd)
             jn = self.q * self.dn * (dnp_list[sp - 2] - dnp_list[sp - 1]) / dx
-            #jp = self.q * self.dp * (dpn_list[1] - dpn_list[0]) / dx
-            #jw = self.q * self.I0 * (1 - self.find_Re(lambd)) * np.exp(-alfa * (self.x - Wp)) - self.q * self.I0 * (1 - self.find_Re(lambd)) * np.exp(-alfa * (self.x + Wn))
-            #jph = jp + jn + jw
             P = self.I0 * self.q * 1.24 / lambd
             S = abs(jn / P)
-            #print(lambd)
-            #print(alfa)
-            #print(f'S = {S}')
             lambd_list.append(lambd)
-            S_list.append(S)
+            Sn_list.append(S)
 
-        '''# Прогоночка для n
+
+        # --------------------------------------------------------------------------------------------------
+
+
+        # Прогоночка для n
         a_list = List_of_param(sn).new_list
         d1_list = List_of_param(sn).new_list
         b_list = List_of_param(sn).new_list
@@ -187,8 +193,15 @@ class Draw_graph(Culculation_param):
         de_list[0] = -d1_list[0] / a_list[0]
         la_list[0] = r_list[0] / a_list[0]
 
-        for lambd in np.arange(0.2, 0.8, 0.1):
-            for j in range (0, 20):
+        Sp_list = list()
+        Sw_list = list()
+
+        for lambd in np.arange(lambd_min, lambda_max, lambda_step):
+            for i in range (0, m):
+                x_list[i] = i * dx
+                G_list[i] = self.find_G_x(x_list[i], self.find_k(lambd), lambd)
+
+            for j in range (0, 1000):
                 for i in range (1, sn - 1):
                     d1_list[i] = 1
                     a_list[i] = -2 - (dx * dx / (self.dp * self.talu))
@@ -204,25 +217,44 @@ class Draw_graph(Culculation_param):
                 for i in range (sp - 2, -1, -1):
                     dpn_list[i] = de_list[i] * dpn_list[i + 1] + la_list[i]
 
-                dpn1_list = dpn_list'''
+                dpn1_list = dpn_list
+
+            # Рассчитываем ток и относительную фоточувствительность. Формируем массивы спектра и фоточувствительности каждоый области
+            k = self.find_k(lambd)
+            alfa = self.find_alfa(k, lambd)
+            P = self.I0 * self.q * 1.24 / lambd
+            jp = self.q * self.dp * (dpn_list[1] - dpn_list[0]) / dx
+            jw = self.q * self.I0 * (1 - self.find_Re(lambd)) * np.exp(-alfa * (self.x * 1E-4 + Wp)) - \
+            self.q * self.I0 * (1 - self.find_Re(lambd)) * np.exp(-alfa * (self.x * 1E-4 - Wn))
+            Sp = abs(jp / P)
+            Sw = abs(jw / P)
+            Sp_list.append(Sp)
+            Sw_list.append(Sw)
+        
+        # тут просто сложение элементов трех массивов, чтоб определить общую S
+        S_list = list(map(lambda x, y, z: x + y + z, Sp_list, Sw_list, Sn_list))
 
         fig, axes = plt.subplots()
 
-        #axes.plot(lambd_list, S_list)
+        axes.plot(lambd_list, Sn_list, label='p+')
+        axes.plot(lambd_list, Sp_list, label='n')
+        axes.plot(lambd_list, Sw_list, label='w')
+        axes.plot(lambd_list, S_list, label='summ')
         #axes.plot(x_list, G_list)
-        axes.plot(xp_list, dnp_list)
+        #axes.plot(xp_list, dnp_list)
         #axes.plot(xn_list, dpn_list)
         #axes.plot(xn_list, Gn_list)
         #axes.plot(xp_list, Gp_list)
         #axes.plot([self.x * 1E-4 + Wn, self.x * 1E-4 + Wn], [G_list[0], G_list[-1]])
         #axes.plot([self.x * 1E-4 - Wp, self.x * 1E-4 - Wp], [G_list[0], G_list[-1]])
 
-        #axes.set_xlim(0, self.d * 1E-4)
-        #axes.set_ylim(0)
+        axes.set_xlim(lambd_min, lambda_max)
+        axes.set_ylim(0)
+        plt.legend(loc=6)
 
-        #plt.title('Распеделение концентрации по глубине залегания', pad = 20)
-        #plt.xlabel('х, см')
-        #plt.ylabel('С, см⁻³')
+        plt.title('Спектральная характеристика относительной \n фоточувствительности фотодиода', pad=10)
+        plt.xlabel('λ, мм')
+        plt.ylabel('S')
 
         # Добавление дополнительной ссетки
         axes.grid(which='major', color = '#666666')
@@ -232,7 +264,6 @@ class Draw_graph(Culculation_param):
         plt.show()
 
 def main():
-    # объявляем объем и запускаем функции рисования
     Ge = Draw_graph()
     Ge.draw()
 
